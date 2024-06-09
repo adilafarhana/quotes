eb1fc5f6735df89b2a61123b20dd50bf997cd36f
adilafarhana <adilafarhanavv@gmail.com>
2024-06-09 23:54:08 +0530

    ok

--- src/component/AddQuotes.jsx
+++ src/component/AddQuotes.jsx
@@ -6,11 +6,16 @@ const AddQuotes = () => {
         {
         "id":"",
         "quote":"",
-        "author":"",
+        "author":""
 
 }
 )
-const inputHandler =(event)
+const inputHandler =(event) =>{
+    changeData({...data,[event.target.name]:event.target.value})
+}
+const readvalue =() => (
+    console.log(data)
+)
 
   return (
     <div>
@@ -21,18 +26,18 @@ const inputHandler =(event)
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">id</label>
-                            <input type="text" className="form-control" name='id' value={data.id} inpur/>
+                            <input type="text" className="form-control" name='id' value={data.id} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">quote</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='quote' value={data.quote} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">author</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='author' value={data.author} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-warning">Submit</button>
+                            <button className="btn btn-warning"onClick={readvalue}>Submit</button>
                         </div>
                     </div>
                 </div>
